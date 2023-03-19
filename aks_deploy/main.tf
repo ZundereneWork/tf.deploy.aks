d8ee5317602712851cb187fe1d530d2a6676cf28
hector21450 <hector21450@gmail.com>
2023-03-19 13:00:55 +0100

    add aks

--- aks_deploy/main.tf
+++ aks_deploy/main.tf
@@ -22,15 +22,51 @@ provider "azurerm" {
 
 
 module "Vnet" {
-    source = "git::https://github.com/ZundereneWork/tf.modules.git//Vnet"
+  source = "git::https://github.com/ZundereneWork/tf.modules.git//Vnet"
 
-    name                = var.name
-    loc                 = var.loc
-    cont                = var.cont 
-    aks_network_cidr    = var.aks_network_cidr
-    location            = var.location
-    resource_group_name = var.resource_group_name
+  name                = var.name
+  loc                 = var.loc
+  cont                = var.cont 
+  aks_network_cidr    = var.aks_network_cidr
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
 
+module "subnet" {
+  depend_on = [module.Vnet]
+  source = "git::https://github.com/ZundereneWork/tf.modules.git//Subnet"
 
-    
+  name                = var.name
+  loc                 = var.loc 
+  cont                = var.cont  
+
+}
+
+module "aks" {
+   depend_on = [module.subnet]
+   source = "git::https://github.com/ZundereneWork/tf.modules.git//aks"
+
+  name                = var.name
+  location            = var.loc
+  resource_group_name = var.resource_group_name
+  version_aks         = var.version_aks
+  service_cidr        = var.service_cidr
+  dns_service_ip      = var.dns_service_ip
+  pod_cidr            = var.pod_cidr
+  size                = var.size
+  node_count          = var.numNodes
+  vm_type             = var.type
+  subnet_id           = module.subnet.id
+  max_pods            = var.maxNode
+  os_disk_size_gb     = var.disk_size_gb
+  agent_subnet_id     = module.subnet.id
+  network_plugin      = var.network_plugin
+  load_balancer_sku   = var.load_balance_sku
+  outbound_type       = var.outbound_type
+  network_policy      = var.network_policy
+  pod_ip_range        = var.ip_range_pod
+  client_id           = var.client_id
+  client_secret       = var.client_secret
+  acr_id              = var.azurerm_container_registry_id
+  admin_group_object_ids = var.list_add_group_ids
 }
