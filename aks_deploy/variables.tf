132bef127d9c90a32c776d780d5ce92719e2f2be
hector21450 <hector21450@gmail.com>
2023-03-27 21:18:38 +0200

    change deploy

--- aks_deploy/variables.tf
+++ aks_deploy/variables.tf
@@ -32,32 +32,69 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "kubernetes_version" {
-  type = string
+variable "name" {
+  description = "Nombre del clúster AKS"
+  type        = string
+}
+
+variable "loc" {
+  description = "Ubicación del clúster AKS"
+  type        = string
+}
+
+variable "cont" {
+  description = "Nombre de la cuenta de almacenamiento"
+  type        = string
+}
+
+variable "location" {
+  description = "Ubicación de los recursos de Azure"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Nombre del grupo de recursos de Azure"
+  type        = string
 }
 
 variable "size" {
-  type = string
+  description = "Tamaño de la máquina virtual para el clúster AKS"
+  type        = string
 }
 
 variable "numNodes" {
-  type = number
+  description = "Número de nodos del grupo de nodos AKS"
+  type        = number
 }
 
+variable "subnet_id" {
+  description = "ID de la subred en la que se creará el clúster AKS"
+  type        = string
+}
 
 variable "disk_size_gb" {
-  type = number
+  description = "Tamaño del disco OS de la máquina virtual para el clúster AKS"
+  type        = number
+}
+
+variable "kubernetes_version" {
+  description = "Versión de Kubernetes para el clúster AKS"
+  type        = string
 }
 
 variable "client_id" {
-  type = string
+  description = "ID del cliente principal"
+  type        = string
 }
 
 variable "client_secret" {
-  type = string
+  description = "Secreto del cliente principal"
+  type        = string
 }
 
 variable "list_add_group_ids" {
-  type = list(string)
+  description = "Lista de IDs de grupo para roles de RBAC"
+  type        = list(string)
 }
+
 
